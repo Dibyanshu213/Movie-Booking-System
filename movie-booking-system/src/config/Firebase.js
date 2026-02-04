644bca51d6db7c359b2809c5bb155195df586976
Dibyanshu213 <dibyanshu40@gmail.com>
2026-02-04 11:28:16 +0545

    ain

--- movie-booking-system/src/config/Firebase.js
+++ movie-booking-system/src/config/Firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 
 // Your Firebase config
 const firebaseConfig = {
- apiKey: "AIzaSyCqv_7YruVbmxJ7pwZ8klCJ1LmRl0C1nMM",
-  authDomain: "movie-booking-system-434e1.firebaseapp.com",
-  projectId: "movie-booking-system-434e1",
-  storageBucket: "movie-booking-system-434e1.firebasestorage.app",
-  messagingSenderId: "236636091486",
-  appId: "1:236636091486:web:b56362947519897d8e30cf",
-  measurementId: "G-F4K255F5N7"
+ apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
